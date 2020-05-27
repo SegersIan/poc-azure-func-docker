c25bc08b703a1afc259b17600acaececc869d7c7
SegersIan <iansegers@gmail.com>
2020-05-27 21:05:27 +0200

    first commit

---
+++ OrchestratorHello/index.js
@@ -0,0 +1,7 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+    outputs.push(yield context.df.callActivity("ActivityHelloWorld", "Tokyo"));
+    return outputs;
+});
