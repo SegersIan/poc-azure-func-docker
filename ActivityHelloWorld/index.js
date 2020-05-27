8b43d7b7707bf8e75f92c3a4c74ac9d083a4aa93
SegersIan <iansegers@gmail.com>
2020-05-27 21:29:55 +0200

    Add helm

--- ActivityHelloWorld/index.js
+++ ActivityHelloWorld/index.js
@@ -1,14 +1,6 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an orchestrator function.
- * 
- * Before running this sample, please:
- * - create a Durable orchestration function
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your
- *   function app in Kudu
- */
+﻿const { run } = require('../lib/helm');
 
 module.exports = async function (context) {
-    return `Hello ${context.bindings.name}!`;
+    const output = await run();
+    return JSON.stringify(output);
 };
--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ FROM mcr.microsoft.com/azure-functions/node:3.0
 ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
     AzureFunctionsJobHost__Logging__Console__IsEnabled=true
 
+COPY bin/linux-amd64/helm /usr/local/bin/helm
 COPY . /home/site/wwwroot
 
 RUN cd /home/site/wwwroot && \
